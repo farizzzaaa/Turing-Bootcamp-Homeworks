7fdc8fc51d2f097b218cecf8dbdbc7b386eaca13
Fariza <felizzz95@mail.ru>
2021-11-12 04:36:02 +0600

    Week 3 Day 1

---
+++ HW/week3day1.js
@@ -0,0 +1,104 @@
+//1.
+
+// let one = 'hello';
+// alert(one);
+
+
+//2.
+// let two = prompt('Привет! Как дела?');
+// alert(two);
+
+//3.
+// let num = 3;
+// let str = '';
+// console.log(Boolean(num));
+// console.log(Boolean(str));
+
+//4.
+// let numOne = prompt("Введите первое число");
+// let numTwo = prompt("Введите второе число");
+// let numSum = Number.parseInt(numOne) + Number.parseInt(numTwo);
+// alert("Общая сумма этих чисел: " + numSum);
+
+
+//5.
+// let numOne = prompt("Введите первое число");
+// let numTwo = prompt("Введите второе число");
+// let numSum = Number.parseInt(numOne) + Number.parseInt(numTwo);
+// let numResidue = Number.parseInt(numOne) % Number.parseInt(numTwo);
+// alert ("Остаток от деления:"+numResidue );
+
+
+//6.
+//  let a = 5;
+// console.log(a++);
+// console.log(a);
+
+// let a = 6;
+// console.log(a--);
+// console.log(a);
+
+//7.
+// let some = prompt("Ведите число-");
+// if (Number.parseInt(some) % 2 === 0) {
+//     alert("Число четное!");
+// }
+// else {
+//     alert("число не четное!")
+// }
+
+//8.
+// let num = 0;
+// let str = '0';
+//
+// let str = "0";
+// let bool = false;
+//Не строгое стравнивает только значение. 1 == "1" будет true, 0 == false будет true.
+//Строгое стравнивает тип 1==="1" будет false, 0 === false будет false
+
+//9.
+// let one = 11
+// console.log( typeof (11));
+//
+// let two = "Hello";
+// console.log(typeof ("Hello"));
+//
+// let three = { a: 1 }
+// console.log(typeof ({ a: 1 }));
+//
+// let four = ""
+// console.log(typeof (null));
+//
+//  let five = false;
+// console.log(typeof (false));
+
+//10.
+
+// const  A = prompt()
+// if (A % 2) {
+//     alert("А не четное");
+// } else {
+//     alert("А четное");
+// }
+
+
+//11.
+
+// let userLog = "feliz";
+// let userPass = "F00";
+//
+// let login = prompt("Введите Ваш логин:");
+// let pass = prompt("Введите Ваш пароль:");
+//
+// if (login !="feliz" || pass !="F00")
+//     alert ("Неверный логин или пароль!" );
+//
+// else {
+//     (login === "feliz" || pass === "F00")
+//     alert("Вы успешно вошли в систему!" )
+// };
+
+//12.
+let a = '30';
+alert(!!'30');
+
