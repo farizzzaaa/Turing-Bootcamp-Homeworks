92ae2ca4b87ed709d6159f2a2dee6a2107c947c9
Fariza <felizzz95@mail.ru>
2021-11-16 00:01:23 +0600

    Week 3 Day 2

---
+++ HW/week3day2.js
@@ -0,0 +1,64 @@
+// 1.
+// function func1 (a, b, c) {
+//     console.log(arguments [0]);
+//     console.log(arguments [1]);
+//     console.log(arguments [2]);
+// }
+// func1(10,20,30)
+
+//2.
+// let one = parseInt(prompt("Первое число"));
+// let two = parseInt(prompt("Второе число"));
+//
+// function func2 (a,b) {
+//     if (a > b) {
+//         return 1;
+//     } else if (a < b) {
+//         return -1;
+//     } else (a = b)
+//     {
+//         return 0;
+//     }
+// }
+//  alert(func2 (one, two ));
+
+//3.
+// let sum = (a, b) => a + b;
+// let subtract = (a, b) => a - b;
+// let divide = (a, b) => a / b;
+// let multi = (a, b) => a * b;
+
+// 4.Не совсем понимаю как сделать(
+
+//
+// console.log("Здравствуйте"+ name)
+//
+// function multiply (name) {
+//     if (name ==="")
+//         console.log("Здравствуйте")
+//     else (name)
+//     console.log("Здравствуйте Гость!")
+//
+// }
+
+
+
+//5.формула Fn = Fn-1 + Fn-2
+// function fib(n) {
+//     return n <= 1 ? n : fib( - 1) + fib( - 2);
+// }
+// alert (fib(10));
+
+
+//6.
+// const fib = n => {
+//     let prev = 0, next = 1;
+//     for(let i = 0; i < n; i++){
+//         let temp = next;
+//         next = prev + next;
+//         prev = temp;
+//     }
+//     return prev;
+// }
+
+
