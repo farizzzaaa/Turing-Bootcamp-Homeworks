0c34bcfd0db17bc8584056ee832c9c8851508f27
Fariza <felizzz95@mail.ru>
2021-11-16 01:33:52 +0600

    Week 3 Day 3.1

---
+++ HW/week3day3.js
@@ -0,0 +1,51 @@
+//1.
+// let a = 0;
+//
+// while (a < 10) {
+//     a++;
+// }
+//
+// do {
+//     a++
+// } while (a < 10);
+//
+// for (a;
+//      a <= 10;
+//      a++);
+
+//2.
+// let one = 0;
+// let two = prompt("Начать!");
+// while (two != "Завершить") {
+//     one++;
+//     two = prompt("Завершить");
+// }
+// console.log(one);
+
+//3.
+//
+// for (let i = 1; i < 20; i += 2) {
+//     console.log(i);
+// }
+
+//5.
+// let a = 1
+// let b = 2
+//
+// let c = a
+// a = b
+// b = c
+// console.log(a);
+// console.log(b);
+
+//6.
+// let num = 143.2343 ;
+// alert(typeof num);
+// let str = String(num);
+// alert(typeof str);
+
+//8.
+let one = "I learn JavaScript";
+console.log(one.indexOf("JavaScript"));
+
+//
